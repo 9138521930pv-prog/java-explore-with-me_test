7381f87e354961432bf46ce8f48280ea1c6f1c3c
9138521930pv-prog <9138521930.pv@gmail.com>
2026-07-07 00:54:28 +0700

    2222

--- main-service/src/main/java/ru/practicum/ewm/model/mappers/LocationMapper.java
+++ main-service/src/main/java/ru/practicum/ewm/model/mappers/LocationMapper.java
@@ -2,12 +2,14 @@
 package ru.practicum.ewm.model.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.practicum.ewm.dto.LocationDto;
 import ru.practicum.ewm.model.Location;
 
 @Mapper(componentModel = "spring")
 public interface LocationMapper {
 
+    @Mapping(target = "id", ignore = true)
     Location toLocation(LocationDto locationDto);
 
     LocationDto toLocationDto(Location location);
--- main-service/src/main/java/ru/practicum/ewm/model/mappers/UserMapper.java
+++ main-service/src/main/java/ru/practicum/ewm/model/mappers/UserMapper.java
@@ -2,6 +2,7 @@ package ru.practicum.ewm.model.mappers;
 
 import org.mapstruct.Mapper;
 
+import org.mapstruct.Mapping;
 import ru.practicum.ewm.dto.NewUserRequest;
 import ru.practicum.ewm.dto.UserDto;
 import ru.practicum.ewm.dto.UserShortDto;
@@ -10,6 +11,7 @@ import ru.practicum.ewm.model.User;
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
+    @Mapping(target = "id", ignore = true)
     User toUser(NewUserRequest newUserRequest);
 
     UserDto toUserDto(User user);
